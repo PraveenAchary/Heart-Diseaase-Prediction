6ba0d5ef1ef10dcc37399921bbe71f873f0ad135
PraveenAchary <praveenvishwabramham@gmail.com>
2026-02-16 22:34:17 +0530

    Add CORS allowed origins for frontend access

--- Backend/HeartDiseasePrediction/settings.py
+++ Backend/HeartDiseasePrediction/settings.py
@@ -130,3 +130,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOWED_ORIGINS = [
+    "https://heart-disease-prediction-iota-ten.vercel.app",
+]
+
